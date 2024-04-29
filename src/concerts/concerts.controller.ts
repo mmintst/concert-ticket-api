7094397fc3800c5a75eb1c8976c8ae306f65ee4b
Salisa <salisa@swtech.co.th>
2024-04-29 20:01:45 +0700

    feat: getConcerts

--- src/concerts/concerts.controller.ts
+++ src/concerts/concerts.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { ConcertsService } from './concerts.service';
 
 @Controller('concerts')
-export class ConcertsController {}
+export class ConcertsController {
+  constructor(private readonly concertsService: ConcertsService) {}
+
+  @Get()
+  getConcerts() {
+    return this.concertsService.getConcerts;
+  }
+
+  @Post()
+  createConcert() {
+    return '';
+  }
+
+  @Patch()
+  updateConcert() {
+    return '';
+  }
+
+  @Delete()
+  removeConcert() {
+    return '';
+  }
+}
--- src/concerts/concerts.service.ts
+++ src/concerts/concerts.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Concert } from './entities/concert.entity';
 
 @Injectable()
-export class ConcertsService {}
+export class ConcertsService {
+  private concerts: Concert[] = [
+    {
+      id: 0,
+      name: 'foo',
+      description: 'bar',
+      totalSeats: 200,
+      reservedSeats: 100,
+      cancelledSeats: 50,
+    },
+    {
+      id: 1,
+      name: 'fizz',
+      description: 'buzz',
+      totalSeats: 300,
+      reservedSeats: 150,
+      cancelledSeats: 70,
+    },
+  ];
+
+  getConcerts(): Concert[] {
+    return this.concerts;
+  }
+}
