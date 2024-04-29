c1d75df176bf6528a5a1f7b1d63387f7e46fad5c
Salisa <salisa@swtech.co.th>
2024-04-29 19:47:18 +0700

    initiate entity controller provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConcertsModule } from './concerts/concerts.module';
 
 @Module({
-  imports: [],
+  imports: [ConcertsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/concerts/entities/concert.entity.ts
@@ -0,0 +1,8 @@
+export class Concert {
+  id: number;
+  name: string;
+  description: string;
+  totalSeats: number;
+  reservedSeats: number;
+  cancelledSeats: number;
+}
